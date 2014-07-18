b8c112e69275d9714127bb2adef7c640e42d3f6e
Tony Fong <tony.hf.fong@gmail.com>
2014-07-18 17:45:17 -0400

    Silent redefining constants warning in constants.jl

--- src/variables.jl
+++ src/variables.jl
@@ -165,7 +165,9 @@ function lintassignment( ex::Expr, ctx::LintContext; islocal = false, isConst=fa
 
     for s in syms
         if in( s, [ :e, :pi, :eu, :catalan, :eulergamma, :golden, :π, :γ, :φ ] )
-            msg( ctx, 1, "You are redefining a mathematical constant " * string(s) )
+            if ctx.file != "constants.jl"
+                msg( ctx, 1, "You are redefining a mathematical constant " * string(s) )
+            end
         end
 
         if islocal
