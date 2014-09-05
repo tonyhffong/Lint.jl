f8a9df9bbe935c530ecda3b8a2d1202442d7eb60
Tony Fong <tony.hf.fong@gmail.com>
2014-09-05 14:51:34 +0700

    simplify lint self code

--- test/lintself.jl
+++ test/lintself.jl
@@ -1,15 +1,3 @@
-if basename( pwd() ) == "Lint"
-    path =  "src/Lint.jl"
-elseif basename( pwd() ) == "src"
-    path = "Lint.jl"
-elseif basename( pwd() ) == "test"
-    path = "../src/Lint.jl"
-else
-    throw( "doesn't know where I am." )
-end
-
 println( "Linting Lint itself")
-println( "Path = ", path )
-msgs = lintfile( path; returnMsgs = true )
-
+msgs = lintpkg( "Lint"; returnMsgs = true )
 @assert( isempty( msgs ) )
