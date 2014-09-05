7f000f29b8482547e023c02438499a2a2d46f120
Tony Fong <tony.hf.fong@gmail.com>
2014-09-05 14:07:15 +0700

    more unused vars tests

--- test/unusedvar.jl
+++ test/unusedvar.jl
@@ -1,7 +1,7 @@
 s = """
 function f(x)
     local a = 1
-    local b = 2
+    local b::Int = 2
     return x+b
 end
 """
@@ -28,3 +28,12 @@ end
 msgs = lintstr(s)
 @test( contains( msgs[1].message, "declared but not used" ))
 @test( contains( msgs[2].message, "declared but not used" ))
+s = """
+function f(x)
+    local a
+    local b = 2
+    return x+b
+end
+"""
+msgs = lintstr(s)
+@test( contains( msgs[1].message, "declared but not used" ))
