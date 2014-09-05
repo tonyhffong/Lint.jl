3b235df0252b3641fef4c4e7f14f45c5869e6ded
Tony Fong <tony.hf.fong@gmail.com>
2014-09-05 12:50:19 +0700

    lambda doesn't support named args. don't test it.

--- test/lambda.jl
+++ test/lambda.jl
@@ -26,7 +26,7 @@ msgs = lintstr(s)
 @assert( contains( msgs[1].message, "Lambda argument" ) )
 s = """
 function f()
-    g  = (x, y::Int, z::Float64=0.0, args...; namedargs... )-> x+1
+    g  = (x, y::Int, z::Float64=0.0, args... )-> x+1
 end
 """
 msgs = lintstr(s)
