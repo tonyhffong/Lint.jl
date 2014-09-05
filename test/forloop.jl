be475998d36a9e62de006a70bf2153423504708b
Tony Fong <tony.hf.fong@gmail.com>
2014-09-05 15:43:00 +0700

    while-false, and multi-var for-loop tests.

--- test/forloop.jl
+++ test/forloop.jl
@@ -7,3 +7,23 @@ end
 """
 msgs = lintstr(s)
 @test( contains( msgs[1].message, "iteration over dictionary uses a (k,v) tuple" ))
+s = """
+function f(x)
+    while false
+        println( "test" )
+    end
+    return x
+end
+"""
+msgs = lintstr(s)
+@test( contains( msgs[1].message, "while false block is unreachable" ))
+s = """
+function f(x)
+    for i in [1,2], j in [4,5]
+        println( i*j)
+    end
+    return x
+end
+"""
+msgs = lintstr(s)
+@test( isempty( msgs ) )
