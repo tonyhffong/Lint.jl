33d4872fdf6c28fef657e7760bef707dc2f8254f
Tony Fong <tony.hf.fong@gmail.com>
2014-09-14 00:39:18 +0700

    Array values lookup tests

--- test/array.jl
+++ test/array.jl
@@ -9,3 +9,38 @@ y = Array[ [1,2], [3,4] ]
 """
 msgs = lintstr( s )
 @test( isempty( msgs ) )
+s = """
+function f( x::Array{Float64,2} )
+    y = x[1,2,3]
+    y
+end
+"""
+msgs = lintstr( s )
+@test( contains( msgs[1].message, "has more indices than dimensions" ) )
+s = """
+function f( x::Array{Float64,2} )
+    x[1,2,3]
+end
+"""
+msgs = lintstr( s )
+@test( contains( msgs[1].message, "has more indices than dimensions" ) )
+s = """
+function f( x::Array{Float64,2} )
+    y = x[:,1]
+    for i in y
+        println( i )
+    end
+end
+"""
+msgs = lintstr( s )
+@test( isempty( msgs ) )
+s = """
+function f( x::Array{Float64,2} )
+    y = x[1,1]
+    for i in y
+        println( i )
+    end
+end
+"""
+msgs = lintstr( s )
+@test( contains( msgs[1].message, "Iteration works for a number" ) )
