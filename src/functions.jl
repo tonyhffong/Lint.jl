fc37d86a41f9ae44887873fe75a54541e0c34583
Tony Fong <tony.hf.fong@gmail.com>
2014-09-14 18:46:42 +0700

    lint-time debug and reminder using lintpragma

--- src/functions.jl
+++ src/functions.jl
@@ -286,7 +286,35 @@ end
 function lintfunctioncall( ex::Expr, ctx::LintContext )
     if ex.args[1] == :lintpragma
         if typeof( ex.args[2] ) <: String
-            push!( ctx.callstack[end].pragmas, ex.args[2] )
+            m = match( r"^((Print)|(Info)|(Warn)|(Error)) ((type)|(me)) +(.+)", ex.args[2] )
+            if m != nothing
+                action = m.captures[1]
+                infotype = m.captures[6]
+                rest = m.captures[9]
+                if infotype == "type"
+                    var = parse( rest )
+                    if isexpr( var, :incomplete )
+                        msg( ctx, 2, "Incomplete expression " * rest )
+                        str = ""
+                    else
+                        str = "typeof( " * rest * " ) == " * string( guesstype( var, ctx ) )
+                    end
+                elseif infotype == "me"
+                    str = rest
+                end
+
+                if action == "Print"
+                    println( str )
+                elseif action == "Info"
+                    msg( ctx, 0, str )
+                elseif action == "Warn"
+                    msg( ctx, 1, str )
+                else
+                    msg( ctx, 2, str )
+                end
+            else
+                push!( ctx.callstack[end].pragmas, ex.args[2] )
+            end
         else
             msg( ctx, 2, "lintpragma must be called using only string literals.")
         end
--- test/pragma.jl
+++ test/pragma.jl
@@ -19,3 +19,71 @@ end
 """
 msgs = lintstr(s)
 @test( length( msgs )==2 )
+
+# lintpragma can also be used to generate messages
+s = """
+function f(x)
+    local b = 2
+    lintpragma( "Info type b")
+    return x+b
+end
+"""
+msgs = lintstr(s)
+@test( length( msgs ) == 1)
+@test( contains( msgs[1].message, "typeof( b ) == Int" ) )
+s = """
+function f(x)
+    local b = 2
+    lintpragma( "Warn type b")
+    return x+b
+end
+"""
+msgs = lintstr(s)
+@test( length( msgs ) == 1)
+@test( contains( msgs[1].message, "typeof( b ) == Int" ) )
+s = """
+function f(x)
+    local b = 2
+    lintpragma( "Error type b")
+    return x+b
+end
+"""
+msgs = lintstr(s)
+@test( length( msgs ) == 1)
+@test( contains( msgs[1].message, "typeof( b ) == Int" ) )
+s = """
+function f(x)
+    local b = 2
+    lintpragma( "Print type b")
+    return x+b
+end
+"""
+msgs = lintstr(s)
+@test( isempty( msgs ) )
+s = """
+function f(x)
+    local b = 2
+    lintpragma( "Print type b[1")
+    return x+b
+end
+"""
+msgs = lintstr(s)
+@test( contains( msgs[1].message, "Incomplete expression " ) )
+s = """
+function f(x)
+    local b = 2
+    lintpragma( "Info me my own reminder")
+    return x+b
+end
+"""
+msgs = lintstr(s)
+@test( contains( msgs[1].message, "my own reminder" ) )
+s = """
+function f(x)
+    local b = 2
+    lintpragma( "Info me " * string( b ) )
+    return x+b
+end
+"""
+msgs = lintstr(s)
+@test( contains( msgs[1].message, "lintpragma must be called using only string literals" ) )
