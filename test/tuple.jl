6b18a9ee4c7b917a6df7b7e2b4b567d4732a2964
Tony Fong <tony.hf.fong@gmail.com>
2014-09-15 16:42:58 +0700

    wrap tuple test in a function

--- test/tuple.jl
+++ test/tuple.jl
@@ -4,7 +4,10 @@ s = """
 msgs = lintstr( s )
 @assert( contains( msgs[1].message, "RHS is a tuple of" ) )
 s = """
+function f()
     (a,b,c) = (1,2,3)
+    return (b,c,a)
+end
 """
 msgs = lintstr( s )
 @assert( isempty( msgs ) )
