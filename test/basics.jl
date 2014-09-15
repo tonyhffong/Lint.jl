8d7e5783c5a600dd54d9824eb3b1b0e7170c3e9b
Tony Fong <tony.hf.fong@gmail.com>
2014-09-15 17:06:25 +0700

    throw test fix

--- test/basics.jl
+++ test/basics.jl
@@ -1,4 +1,4 @@
 p = "non_existing_1234_4321"
 @test !ispath( p )
-@test_throws( ErrorException, lintfile( p ) )
-@test_throws( ErrorException, lintpkg( p ) )
+@test_throws( String, lintfile( p ) )
+@test_throws( String, lintpkg( p ) )
