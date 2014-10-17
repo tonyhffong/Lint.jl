ca4b14df1c2095e865fc059aa9c02565fbfe5802
Tony Fong <tony.hf.fong@gmail.com>
2014-10-17 20:30:49 +0700

    clean up dict syntax test

--- test/dictkey.jl
+++ test/dictkey.jl
@@ -1,65 +1,37 @@
-s = """
-[ :a=>1, :b=>2, :a=>3]
-"""
-msgs = lintstr( s )
+if VERSION < v"0.4-"
+    s = """
+    [ :a=>1, :b=>2, :a=>3]
+    """
+    msgs = lintstr( s )
+    @test( contains( msgs[1].message, "may be deprecated by Julia 0.4" ) )
 
+    s = """
+    { :a=>1, :b=>2, :a=>3}
+    """
+    msgs = lintstr( s )
+    @test( contains( msgs[1].message, "may be deprecated by Julia 0.4" ) )
+end
+
+s = """@compat Dict(:a=>1, :b=>2, :a=>3 )"""
+msgs = lintstr(s)
 @test( contains( msgs[1].message, "Duplicate key" ) )
-if VERSION < v"0.4-"
-    @test( contains( msgs[2].message, "may be deprecated by Julia 0.4" ) )
-end
+@test( contains( msgs[2].message, "Use explicit Dict{K,V}() for better performances" ) )
 
 s = """
-{ :a=>1, :b=>2, :a=>3}
+@compat Dict( :a=>Date( 2014,1,1 ), :b=>Date( 2015,1,1 ) )
 """
 msgs = lintstr( s )
-
-@test( contains( msgs[1].message, "Duplicate key" ) )
-if VERSION < v"0.4-"
-    @test( contains( msgs[2].message, "@compat Dict{K,V}(a=>b,...) for better performances" ) )
-end
+@test( contains( msgs[1].message, "Use explicit Dict{K,V}() for better performances" ) )
 
 s = """
-{ :a=>Date( 2014,1,1 ), :b=>Date( 2015,1,1 ) }
-"""
-msgs = lintstr( s )
-if VERSION < v"0.4-"
-    @test( contains( msgs[1].message, "Use explicit @compat Dict{K,V}(a=>b,...) for better performances" ) )
-end
-
-s = """
-[ :a=>1, :b=>"" ]
+@compat Dict{Symbol,Int}( :a=>1, :b=>"" )
 """
 msgs = lintstr( s )
 @test( contains( msgs[1].message, "Multiple value types detected" ) )
-if VERSION < v"0.4-"
-    @test( contains( msgs[2].message, "may be deprecated by Julia 0.4" ) )
-end
 
 s = """
-[ :a=>1, "b"=>2 ]
+@compat Dict{Symbol,Int}( :a=>1, "b"=>2 )
 """
 msgs = lintstr( s )
 @test( contains( msgs[1].message, "Multiple key types detected" ) )
-if VERSION < v"0.4-"
-    @test( contains( msgs[2].message, "may be deprecated by Julia 0.4" ) )
-end
 
-s = """
-(Symbol=>Int)[ :a=>1, :b=>2 ]
-"""
-msgs = lintstr( s )
-if VERSION < v"0.4-"
-    @test( contains( msgs[1].message, "may be deprecated by Julia 0.4" ) )
-else
-    @test( isempty( msgs ) )
-end
-
-s = """
-(Any=>Any)[ :a=>1, :b=>2 ]
-"""
-msgs = lintstr( s )
-if VERSION < v"0.4-"
-    @test( contains( msgs[1].message, "may be deprecated by Julia 0.4" ) )
-else
-    @test( isempty( msgs ) )
-end
--- test/funcall.jl
+++ test/funcall.jl
@@ -160,7 +160,7 @@ msgs = lintstr(s)
 s = """
 g( ;x=0, y=1 ) = x+y
 function f()
-    d = @compat( Dict(:x=>6,:y=>4 ) )
+    d = @compat( Dict{Symbol,Int}(:x=>6,:y=>4 ) )
     g(; d... )
 end
 """
