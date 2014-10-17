989a21745fb2e22fe28d914ffc65164b9349ce7e
Tony Fong <tony.hf.fong@gmail.com>
2014-10-17 20:31:31 +0700

    comprehension test fix

--- test/comprehensions.jl
+++ test/comprehensions.jl
@@ -3,13 +3,17 @@ s = """[i for i in 1:2]
 msgs = lintstr( s )
 @assert( isempty( msgs ) )
 
-s = """{i for i in 1:2}
-"""
-msgs = lintstr( s )
 if VERSION < v"0.4-"
+    s = """{i for i in 1:2}
+    """
+    msgs = lintstr( s )
     @assert( contains( msgs[1].message, "deprecated by Julia 0.4" ) )
-else
-    @assert( isempty( msgs ) )
+
+    s = """
+    { y1 => y1*y1 for y1 in 1:2 }
+    """
+    msgs = lintstr( s )
+    @assert( contains( msgs[1].message, "deprecated by Julia 0.4" ) )
 end
 
 s = """
@@ -17,16 +21,6 @@ s = """
 """
 msgs = lintstr( s )
 @assert( isempty( msgs ) )
-
-s = """
-{ y1 => y1*y1 for y1 in 1:2 }
-"""
-msgs = lintstr( s )
-if VERSION < v"0.4-"
-    @assert( contains( msgs[1].message, "deprecated by Julia 0.4" ) )
-else
-    @assert( isempty( msgs ) )
-end
 
 s = """
 (Int=>Int)[ y2 => y2*y2 for y2 in 1:2 ]
