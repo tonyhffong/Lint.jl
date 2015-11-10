f5dd434cee48d67a91428e3348cb35a96629f584
Michael Klassen <michael.klassen@invenia.ca>
2015-11-10 15:14:28 -0600

    Add tests for server

---
+++ test/server.jl
@@ -0,0 +1,24 @@
+# find a good port
+conn = listenany(2228)
+close(conn[2])
+port = conn[1]
+
+@async lintserver(port)
+sleep(1) #let sever start
+
+
+conn = connect(port)
+write(conn, "empty\n")
+write(conn, "1\n")
+write(conn, "\n")
+
+@test readline(conn) == "\n"
+
+
+conn = connect(port)
+write(conn, "undeclared_symbol\n")
+write(conn, "4\n")
+write(conn, "bad\n")
+
+@test contains(readline(conn), "Use of undeclared symbol bad\n")
+@test readline(conn) == "\n"
