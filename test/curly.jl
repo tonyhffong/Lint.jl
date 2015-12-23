27a57a768956a01bf481baab26d0a356077b8186
Fengyang Wang <fengyang.wang.0@gmail.com>
2015-12-23 00:14:41 -0500

    Better curly linting

--- test/curly.jl
+++ test/curly.jl
@@ -2,26 +2,44 @@ s = """
 a = Dict{ :Symbol, Any}
 """
 msgs = lintstr(s)
-@test( contains( msgs[1].message, "Probably illegal use of" ) )
+@test( contains( msgs[1].message, "type parameter for Dict" ) )
 
 s = """
 a = Dict{ :Symbol, Any}()
 """
 msgs = lintstr(s)
-@test( contains( msgs[1].message, "Probably illegal use of" ) )
+@test( contains( msgs[1].message, "type parameter for Dict" ) )
+
+s = """
+a = Set{ Tuple{ Int, Int } }()
+"""
+msgs = lintstr(s)
+@test( isempty( msgs ) )
 
 s = """
 a = Set{ ( Int, Int ) }()
 """
 msgs = lintstr(s)
-@test( isempty( msgs ) )
+@test contains( msgs[1].message, "Probably illegal use of" )
 
 s = """
 b = :Symbol
 a = Dict{ b, Any}()
 """
 msgs = lintstr(s)
-@test( contains( msgs[1].message, "Probably illegal use of" ) )
+@test( contains( msgs[1].message, "type parameter for Dict" ) )
+
+s = """
+a = Array{2, Int64}()
+"""
+msgs = lintstr(s)
+@test contains( msgs[1].message, "type parameter for Array" )
+
+s = """
+a = Array{Int64, 5, 5}()
+"""
+msgs = lintstr(s)
+@test contains( msgs[1].message, "Too many type parameters" )
 
 s = """
 a = Ptr{ Void }
@@ -34,4 +52,3 @@ s = """
 """
 msgs = lintstr( s )
 @test( isempty( msgs ) )
-
