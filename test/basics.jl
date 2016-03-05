0e3e6aec53917c87e51edd8395f40a164d3a11a9
tonyhffong <tony.hf.fong@gmail.com>
2016-03-04 20:26:08 -0500

    minor test fix

--- test/basics.jl
+++ test/basics.jl
@@ -1,7 +1,13 @@
 p = "non_existing_1234_4321"
 @test !ispath(p)
+
+if VERSION < v"0.5-"
 @test_throws(AbstractString, lintfile(p))
 @test_throws(AbstractString, lintpkg(p))
+else
+@test_throws(ASCIIString, lintfile(p))
+@test_throws(UTF8String, lintpkg(p))
+end
 
 # Lint package with full path
 msgs = lintpkg(joinpath(Pkg.dir("Lint"), "test", "FakePackage"); returnMsgs = true)
