82fdf2d3e1d0560d9be4993b54fdee2cc6142d44
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2018-11-30 15:09:06 -0300

    Break `guesstype` into multi-dispatch code.
    This will make testing much easier.
    
    Inspired on
    https://pixorblog.wordpress.com/2018/02/23/julia-dispatch-enum-vs-type-comparison/ (or
    https://www.juliabloggers.com/julia-dispatching-enum-versus-type/ )

--- src/guesstype.jl
+++ src/guesstype.jl
@@ -51,62 +51,112 @@ function guesstype(ex::Symbol, ctx::LintContext)
     end
 end
 
-function guesstype(ex::Expr, ctx::LintContext)::Type
-    ex = ExpressionUtils.expand_trivial_calls(ex)
+# Tag definitions
+abstract type ExpressionTag end
+struct TupleTag <: ExpressionTag end
+struct ColonTag <: ExpressionTag end
+struct BlockTag <: ExpressionTag end
+struct ReturnTag <: ExpressionTag end
+struct MacroCallTag <: ExpressionTag end
+struct CallTag <: ExpressionTag end
+struct RefTag <: ExpressionTag end
+struct ComparisonTag <: ExpressionTag end
+struct CurlyTag <: ExpressionTag end
+struct TernaryIfTag <: ExpressionTag end
+struct LambdaTag <: ExpressionTag end
+struct DefaultTag <: ExpressionTag end
 
+# condition → tag
+# TODO(felipe): Use Match.jl
+function get_tag_per_condition(ex::Expr)
     if isexpr(ex, :tuple)
-        ts = Type[]
-        for a in ex.args
-            push!(ts, guesstype(a, ctx))
-        end
-        return Tuple{ts...}
+        return TupleTag
     end
-
     if isexpr(ex, :(::)) && length(ex.args) == 2
-        return parsetype(ctx, ex.args[2])
+        return ColonTag
     end
-
     if isexpr(ex, :block)
-        return isempty(ex.args) ? Nothing : guesstype(ex.args[end], ctx)
+        return BlockTag
     end
-
     if isexpr(ex, :return)
-        tmp = guesstype(ex.args[1], ctx)
-        return tmp
+        return ReturnTag
     end
-
     if isexpr(ex, :call)
-        fn = ex.args[1]
-        if any(x -> isexpr(x, :kw) || isexpr(x, :(...)), ex.args[2:end])
-            # TODO: smarter way to deal with kw/vararg
-            return Any
-        end
-        argtypes = map(x -> guesstype(x, ctx), ex.args[2:end])
-
-        # infer return types of Base functions
-        obj = abstract_eval(ctx, fn)
-        type_argtypes = [isa(t, Type) ? t : Any for t in argtypes]
-        if obj ≠ nothing
-            inferred = StaticTypeAnalysis.infertype(obj, type_argtypes)
-            if inferred ≠ Any
-                return inferred
-            end
-        end
+        return CallTag
     end
-
     if isexpr(ex, :macrocall)
-        if ex.args[1] == Symbol("@sprintf")
-            return String
-        elseif ex.args[1] == Symbol("@compat")
-            return guesstype(ex.args[2], ctx)
+        return MacroCallTag
+    end
+    if isexpr(ex, :ref) # it could be a ref a[b] or an array Int[1,2,3], Vector{Int}[]
+        return RefTag
+    end
+    if isexpr(ex, :comparison)
+        return ComparisonTag
+    end
+    if isexpr(ex, :curly)
+        return CurlyTag
+    end
+    # simple if statement e.g. test ? 0 : 1
+    if isexpr(ex, :if) && 2 ≤ length(ex.args) ≤ 3
+        return TernaryIfTag
+    end
+
+    if isexpr(ex, :(->))
+        return LambdaTag
+    end
+
+    return DefaultTag
+end
+
+# Specializations for each type below
+function guesstype(ex::Expr, ctx::LintContext, ::Type{TupleTag})::Type
+    ts = Type[]
+    for a in ex.args
+        push!(ts, guesstype(a, ctx))
+    end
+    return Tuple{ts...}
+end
+
+function guesstype(ex::Expr, ctx::LintContext, ::Type{ColonTag})::Type
+    parsetype(ctx, ex.args[2])
+end
+
+function guesstype(ex::Expr, ctx::LintContext, ::Type{BlockTag})::Type
+    isempty(ex.args) ? Nothing : guesstype(ex.args[end], ctx)
+end
+function guesstype(ex::Expr, ctx::LintContext, ::Type{ReturnTag})::Type
+    guesstype(ex.args[1], ctx)
+end
+
+function guesstype(ex::Expr, ctx::LintContext, ::Type{CallTag})::Type
+    fn = ex.args[1]
+    if any(x -> isexpr(x, :kw) || isexpr(x, :(...)), ex.args[2:end])
+        # TODO: smarter way to deal with kw/vararg
+        return Any
+    end
+    argtypes = map(x -> guesstype(x, ctx), ex.args[2:end])
+
+    # infer return types of Base functions
+    obj = abstract_eval(ctx, fn)
+    type_argtypes = [isa(t, Type) ? t : Any for t in argtypes]
+    if obj ≠ nothing
+        inferred = StaticTypeAnalysis.infertype(obj, type_argtypes)
+        if inferred ≠ Any
+            return inferred
         end
     end
+    return Any
+end
 
-    if isexpr(ex, :curly)
-        return Type
+function guesstype(ex::Expr, ctx::LintContext, ::Type{MacroCallTag})::Type
+    if ex.args[1] == Symbol("@sprintf")
+        return String
+    elseif ex.args[1] == Symbol("@compat")
+        return guesstype(ex.args[2], ctx)
     end
+end
 
-    if isexpr(ex, :ref) # it could be a ref a[b] or an array Int[1,2,3], Vector{Int}[]
+function guesstype(ex::Expr, ctx::LintContext, ::Type{RefTag})::Type
         if isexpr(ex.args[1], :curly) # must be a datatype, right?
             elt = abstract_eval(ctx, ex.args[1])
             if elt ≠ nothing && isa(elt, Type)
@@ -175,35 +225,45 @@ function guesstype(ex::Expr, ctx::LintContext)::Type
             end
             return inferred
         end
-    end
+end
 
-    if isexpr(ex, :comparison)
-        return Bool
-    end
+function guesstype(ex::Expr, ctx::LintContext, ::Type{ComparisonTag})::Type
+    Bool
+end
 
-    # simple if statement e.g. test ? 0 : 1
-    if isexpr(ex, :if) && 2 ≤ length(ex.args) ≤ 3
-        tt = guesstype(ex.args[2], ctx)
-        ft = if length(ex.args) == 3
-            guesstype(ex.args[3], ctx)
-        else
-            nothing
-        end
-        if tt == ft
-            # we need this case because tt and ft might be symbols
-            return tt
-        elseif isa(tt, Type) && isa(ft, Type)
-            return Union{tt, ft}
-        else
-            return Any
-        end
-    end
+function guesstype(ex::Expr, ctx::LintContext, ::Type{CurlyTag})::Type
+    Type
+end
 
-    if isexpr(ex, :(->))
-        return Function
+function guesstype(ex::Expr, ctx::LintContext, ::Type{TernaryIfTag})::Type
+    tt = guesstype(ex.args[2], ctx)
+    ft = if length(ex.args) == 3
+        guesstype(ex.args[3], ctx)
+    else
+        Nothing
     end
+    if tt == ft
+        # we need this case because tt and ft might be symbols
+        return tt
+    elseif isa(tt, Type) && isa(ft, Type)
+        return Union{tt, ft}
+    else
+        return Any
+    end
+end
 
-    return Any
+function guesstype(ex::Expr, ctx::LintContext, ::Type{LambdaTag})::Type
+    Function
+end
+
+function guesstype(ex::Expr, ctx::LintContext, ::Type{DefaultTag})::Type
+    Any
+end
+
+# `guesstype` dispatcher (resolves the tag)
+function guesstype(ex::Expr, ctx::LintContext)::Type
+    ex = ExpressionUtils.expand_trivial_calls(ex)
+    return guesstype(ex, ctx, get_tag_per_condition(ex))
 end
 
 guesstype(ex, ctx::LintContext) = lexicaltypeof(ex)
