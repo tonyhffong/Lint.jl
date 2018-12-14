66fc1aa48ad2889fe22c64db6c3d0e2016e8f1d5
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2018-12-14 10:48:41 -0300

    Reformat dict tests

--- test/dictkey.jl
+++ test/dictkey.jl
@@ -1,22 +1,22 @@
-s = """
+@testset "Dictionaries" begin
+    let s = """
 Dict(:a=>1, :b=>2, :a=>3)
-"""
-msgs = lintstr(s)
-@test msgs[1].code == :E334
-@test occursin("duplicate key in Dict", msgs[1].message)
+""",
+        msgs = lintstr(s)
+        @test msgs[1].code == :E334
+    end
 
-s = """
+    let s = """
 Dict{Symbol,Int}(:a=>1, :b=>"")
-"""
-msgs = lintstr(s)
-@test msgs[1].code == :E532
-@test occursin("multiple value types detected. Use Dict{K,Any}(, msgs[1].message) for " *
-    "mixed type dict")
+""",
+        msgs = lintstr(s)
+        @test msgs[1].code == :E532
+    end
 
-s = """
+    let s = """
 Dict{Symbol,Int}(:a=>1, "b"=>2)
-"""
-msgs = lintstr(s)
-@test msgs[1].code == :E531
-@test occursin("multiple key types detected. Use Dict{Any,V}(, msgs[1].message) for " *
-    "mixed type dict")
+""",
+        msgs = lintstr(s)
+        @test msgs[1].code == :E531
+    end
+end
