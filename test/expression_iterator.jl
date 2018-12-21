b34c6053c7cd5de5bc1ce6b58055740b6d1997b2
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2018-12-21 11:27:52 -0300

    Rewrite test

--- test/expression_iterator.jl
+++ test/expression_iterator.jl
@@ -1,16 +1,9 @@
 @testset "Expression iterator" begin
-    let s="""
-a
-b
-c
-í
-ω
-゛
-""",
-        line_it=Lint.ExpressionIterator.each_line_iterator(s),
-        lines=collect(line_it)
-        @test eltype(lines) == SubString{String} # need the indices to original string
-        @test lines == ["a", "b", "c", "í", "ω", "゛"]
+    let line_array=["a", "b", "c", "í", "ω", "゛"],
+        lines_string=join(line_array, "\n"),
+        line_it=Lint.ExpressionIterator.each_line_iterator(lines_string),
+        collected_lines=collect(line_it)
+        @test eltype(collected_lines) == SubString{String} # need the indices to original string
+        @test collected_lines == line_array
     end
-
 end
