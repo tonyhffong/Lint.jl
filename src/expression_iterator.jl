217809a9a7bf22e2f5cce91e2cf2ab079447edb5
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2018-12-21 14:34:24 -0300

    Inline functions, use offsets as lines

--- src/expression_iterator.jl
+++ src/expression_iterator.jl
@@ -1,66 +1,79 @@
 module ExpressionIterator
 """Iterator of Substrings on which each contains a line"""
-function each_line_iterator(s::AbstractString)
-    Channel(c->begin
+each_line_iterator(s::AbstractString) = Channel(c->begin
             for substring in split(s, "\n", limit=0, keepempty=false)
                 put!(c, substring)
             end
             end, ctype=SubString{String})
-end
 
 struct EachExpression
     original::String
-    current_line_offset::Int
+    current_line_offset::Int # used externally
 end
 
+""" Get [begin, end] offsets for each line (end is inclusive)"""
+lines_offsets(s::AbstractString) = map(line->begin
+                                       real_offset=line.offset +1 # SubString.offset + 1 ↔ String.index
+                                       (real_offset, real_offset + length(line) -1) # TODO(felipe) use `prevind` ?
+                                       end, each_line_iterator(s))
+
+"""points to info about "where should we get our *next* expression"""
 struct _EachExpressionState
-    next_line_it # `iterate(each_line_iterator(…))`
-    offset_where_last_expression_ends::Union{Int,Nothing}
+    next_line_offset # `iterate(lines_offsets(…))`
+    offset_where_last_expression_ends::Union{Nothing,Int}
 end
+
 _EachExpressionState(iter::EachExpression) = _EachExpressionState(
-    Base.iterate(each_line_iterator(iter.original)),
+    Base.iterate(lines_offsets(iter.original)),
     nothing)
 
-offset_lines(s::AbstractString) = map(line->begin
-                                      (line, line.offset +1) # SubString.offset + 1 ↔ String.index
-                                      end, each_line_iterator(s))
-
 function Base.iterate(iter::EachExpression, state=_EachExpressionState(iter)) #::Union{Nothing, Tuple{EachExpression, _EachExpressionState}}
-    if state.next_line !== nothing
+    if state.line_offset_it == nothing
         # no more lines → we're done
         return nothing
     end
 
-    (line_it, line_state) = state.next_line_it
-    current_line_offset=line.offset + 1
-    next_line = line_it
-    if offset_where_last_expression_ends !== nothing
-        # move line-ward forward until we catch up with last parsed expression
-        while current_line_offset < offset_where_last_expression_ends
-            if next_line isa Nothing
-                # ran out of lines with the last expression
+    # line ↔ [begin, end] offsets that describe a line
+
+    # move next-line-wards …
+    (current_line, current_line_state) = state.line_offset_it
+    @show state.line_offset_it, current_line
+    (line_begin, line_end) = current_line
+    next_line_it = iterate(state.line_offset_it, current_line_state)
+
+    # … at least we catch up with last parsed expression
+    if state.offset_where_last_expression_ends !== nothing
+        while line_begin < state.offset_where_last_expression_ends
+            if next_line_it isa Nothing
+                # ran out of lines with the last expression → nothing left to parse
                 return nothing
             end
-            next_line = iterate(line_it, line_state)
+            # update current line info
+            (current_line, current_line_state) = next_line_it
+            (line_begin, line_end) = current_line
+
+            # update next line info
+            next_line_it = iterate(next_line_it, current_line_state)
+            @show current_line, next_line_it
         end
     end
 
-    try
-        # `i` will be sliding index of start-for-current-expression
-        i = iter.current_line_offset
-        while i ≤ length(current_line)
-            (ex, i_for_end_of_expression) = Meta.parse(full_string, i)
-            return (ex, _EachExpressionState(next_line, i_for_end_of_expression))
-        end
-    catch y
-        # report an unexpected error
-        # end-of-input and parsing errors are expected
-        if typeof(y) != Meta.ParseError || y.msg != "end of input"
-            msg(context, :E111, string(y))
-        end
-    end
+    @show line_begin, line_end
+    @show "trying to parse ", SubString(iter.original, line_begin, line_end)
+    (ex, i_for_end_of_expression) = Meta.parse(iter.original, line_begin)
+
+    return (ex, _EachExpressionState(next_line_it,
+                                     i_for_end_of_expression))
 end
 
 each_expression(original::String, current_line_offset=0) = EachExpression(original, current_line_offset)
+
+function Base.length(iter::EachExpression)
+    count=0
+    for ex in iter
+        count += 1
+    end
+    count
+end
 
 end # module ExpressionIterator
--- test/expression_iterator.jl
+++ test/expression_iterator.jl
@@ -1,16 +1,29 @@
 @testset "Expression iterator" begin
-    let line_array=["a", "b", "c", "í", "ω", "゛"],
-        lines_string=join(line_array, "\n"),
-        line_it=Lint.ExpressionIterator.each_line_iterator(lines_string),
-        collected_lines=collect(line_it)
+    let line_array = ["a", "b", "c", "í", "ω", "゛"],
+        lines_string = join(line_array, "\n"),
+        line_it = Lint.ExpressionIterator.each_line_iterator(lines_string),
+        collected_lines = collect(line_it)
         @test eltype(collected_lines) == SubString{String} # need the indices to original string
         @test collected_lines == line_array
     end
+
     let line_array=collect(map(i->"$i", 1:9)),
         lines_string=join(line_array, "\n"),
-        lines_with_offsets = collect(Lint.ExpressionIterator.offset_lines(lines_string)),
-        expected_offsets = [i*2-1 for i in 1:9],
-        expected_lines_with_offsets=collect(zip(line_array, expected_offsets))
-        @test lines_with_offsets == expected_lines_with_offsets
+        lines_offsets = collect(Lint.ExpressionIterator.lines_offsets(lines_string)),
+        expected_offsets = [(i*2-1,i*2-1) for i in 1:9] # "1", "\n", … "7", "\n" …
+        @test lines_offsets == expected_offsets
+        @test [lines_string[r[1]:r[2]] for r in lines_offsets] == line_array
+    end
+
+    let s = """
+            function f()
+            x = 0
+            x
+            end
+            """,
+        lines = collect(Lint.ExpressionIterator.each_line_iterator(s))
+        lines_offsets = collect(Lint.ExpressionIterator.lines_offsets(s))
+        lines_per_offsets = [SubString(s, t[1], t[2]) for t in lines_offsets]
+        @test lines == [SubString(s, t[1], t[2]) for t in lines_offsets]
     end
 end
