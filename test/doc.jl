2e9744806931f043fb5e54d240fba91a6907df30
Felipe Lema <1232306+FelipeLema@users.noreply.github.com>
2019-05-07 14:23:20 -0400

    wrap @doc test in a @testset

--- test/doc.jl
+++ test/doc.jl
@@ -1,50 +1,53 @@
-s = """
-@doc "this is a test" -> f() = 0
-"""
-msgs = lintstr(s)
-@test isempty(msgs)
+@testset "@doc" begin
+    s = """
+    @doc "this is a test" -> f() = 0
+    """
+    msgs = lintstr(s)
+    @test isempty(msgs)
 
-s = "@doc \"\"\"this is a test\"\"\" -> f() = 0"
-msgs = lintstr(s)
-@test isempty(msgs)
+    s = "@doc \"\"\"this is a test\"\"\" -> f() = 0"
+    msgs = lintstr(s)
+    @test isempty(msgs)
 
-s = """
-@doc "this is a test" f() = 0
-"""
-msgs = lintstr(s)
-@test isempty(msgs)
+    s = """
+    @doc "this is a test" f() = 0
+    """
+    msgs = lintstr(s)
+    @test isempty(msgs)
 
-s = """
-@doc \"\"\"
-this is a test
-\"\"\" f() = 0
-"""
-msgs = lintstr(s)
-@test isempty(msgs)
+    s = """
+    @doc \"\"\"
+    this is a test
+    \"\"\" f() = 0
+    """
+    msgs = lintstr(s)
+    @test isempty(msgs)
 
-s = """
-@doc f
-"""
-msgs = lintstr(s)
-@test isempty(msgs)
+    s = """
+    @doc f
+    """
+    msgs = lintstr(s)
+    @test isempty(msgs)
 
-s = """
-func(v) = v
+    s = """
+    func(v) = v
 
-\"\"\"
-Documentation
-\"\"\"
-func(v)
-"""
-msgs = lintstr(s)
-@test isempty(msgs)
+    \"\"\"
+    Documentation
+    \"\"\"
+    func(v)
+    """
+    msgs = lintstr(s)
+    @test isempty(msgs)
 
-# TODO: should we warn this documentation?
-"""
-type TestType
+    # TODO: should we warn this documentation?
+    """
+    type TestType
     "doc"
     a
+    end
+    """
+    msgs = lintstr(s)
+    @test isempty(msgs)
 end
-"""
-msgs = lintstr(s)
-@test isempty(msgs)
+
